bca7535910b825941e7a720f5ec1366ca8a3dcca
KnockOut19882 <mpodolski1988@gmail.com>
2025-06-18 20:08:31 +0200

    Add js

--- js/script.js
+++ js/script.js
@@ -8,15 +8,6 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-function printMessage(msg){
-    var div = document.createElement('div');
-    div.innerHTML = msg;
-    document.getElementById('messages').appendChild(div);
-}
-
-function clearMessages(){
-    document.getElementById('messages').innerHTML = '';
-}
 
 window.onload = function() {
 
