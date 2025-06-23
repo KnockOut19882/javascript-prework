a9ff37392dbc76c94e02b1844e6a6fd47e82d25a
KnockOut19882 <mpodolski1988@gmail.com>
2025-06-23 09:58:52 +0200

    Change var to const

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ window.onload = function() {
     console.log('ruch gracza to: ' + playerMove);
     const randomNumber = Math.floor(Math.random() * 3 + 1);
     console.log('wylosowana liczba to: ' + randomNumber);
-    var computerMove = getMoveName(randomNumber);
+    const computerMove = getMoveName(randomNumber);
     console.log('ruch komputera to: ' + computerMove);
     displayResult(playerMove, computerMove);
     printScore();
